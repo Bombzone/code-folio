8cf34003c8d90fdc3ac7b7a9f60d2399c6056437
Ryan Posey <ryan.m.posey@gmail.com>
2025-09-12 22:15:14 -0600

    url update

--- components.js
+++ components.js
@@ -9,28 +9,28 @@ console.log(window.location.pathname);
 
 
 switch (window.location.pathname) {
-    case "/":
-    case "/index.html":
+    case "/code-folio/":
+    case "/code-folio/index.html":
         console.log("here");
         $("#home").addClass('active').attr("aria-current", "page");
         break
-    case "/about.html":
+    case "/code-folio/about.html":
         $("#about").addClass('active').attr("aria-current", "page");
 
         break;
-    case "/projects.html":
+    case "/code-folio/projects.html":
         $("#projects").addClass('active').attr("aria-current", "page");
 
         break;
-    case "/accomplishments.html":
+    case "/code-folio/accomplishments.html":
         $("#accomplishments").addClass('active').attr("aria-current", "page");
 
         break;
-    case "/resume.html":
+    case "/code-folio/resume.html":
         $("#resume").addClass('active').attr("aria-current", "page");
 
         break;
-    case "/contact.html":
+    case "/code-folio/contact.html":
 
         break;
     default:
